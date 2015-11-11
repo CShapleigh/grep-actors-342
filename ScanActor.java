c47d29a643bf9fb4f16e32ff50d85d90fc3a2b76
Piper Chester <piperchester@gmail.com>
2015-11-11 13:42:02 -0500

    Make ScanActor static

--- ScanActor.java
+++ ScanActor.java
@@ -1,7 +1,6 @@
 import akka.actor.UntypedActor;
  
-public class ScanActor extends UntypedActor {
- 
+public static class ScanActor extends UntypedActor {
   public void onReceive(Object message) throws Exception {
   	// TODO: implement
   }
