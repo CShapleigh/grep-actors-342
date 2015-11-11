9a0b58d18a86e6f87770f83b88343a8cf96d37b1
Sean Dolan <sed9182@rit.edu>
2015-11-11 13:20:35 -0500

    added configure message object

---
+++ Configure.java
@@ -0,0 +1,15 @@
+static class Configure{
+	private final CollectionActor act;
+	private final String file;
+	
+	public Configure(CollectionActor act, String file){
+		this.act = act;
+		this.file = file;
+	}
+	public String getFile(){
+		return file;
+	}
+	public CollectionActor getActor(){
+		return act;
+	}
+}
