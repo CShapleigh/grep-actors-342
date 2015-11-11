5ff944867233066bb694ed948d5df1d0924a0830
Piper Chester <piperchester@gmail.com>
2015-11-11 13:24:30 -0500

    Skeleton CGrep

---
+++ CGrep.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.concurrent.*;
+
+public class CGrep {
+  public static void main(String [] args) {
+	  final String pattern = args[0];
+	  ArrayList<String> files = new ArrayList<String>();
+	  for (int i = 1; i < args.length; i++) {
+		  files.add(args[i]);
+	  }
+
+	  for (final String file : files) {
+	  	// TODO: Start CGREP
+	  }
+  }
+}
