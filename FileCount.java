15ef56eb7d47bc92826fa02e52a4574cb0e13c73
Charles Shapleigh <CShapleigh@gmail.com>
2015-11-11 13:20:41 -0500

    Filecount

---
+++ FileCount.java
@@ -0,0 +1,12 @@
+public class FileCount {
+  public final int filesBeingScanned;
+
+  public FileCount (int files) {
+    filesBeingScanned = files;
+  }
+
+  public getFilesBeingScanned() {
+    return filesBeingScanned;
+  }
+
+}
